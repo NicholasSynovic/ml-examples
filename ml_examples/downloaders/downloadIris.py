edc27f55443311d9dc055a0b421ccd9cf29f7e88
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-16 16:45:26 -0500

    Add code to download the iris data set

---
+++ ml_examples/downloaders/downloadIris.py
@@ -0,0 +1,22 @@
+from pathlib import PurePath
+
+from progress.bar import Bar
+from requests import Response, get
+
+dataURL: str = (
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+)
+
+
+def main() -> None:
+    dataPath: PurePath = PurePath("../data/iris.data")
+
+    dataResp: Response = get(dataURL)
+
+    with open(dataPath, mode="wb") as dataFile:
+        dataFile.write(dataResp.content)
+        dataFile.close()
+
+
+if __name__ == "__main__":
+    main()
